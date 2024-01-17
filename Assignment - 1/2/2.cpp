c1e35839aa23b7f9443eaf1c8c0c8015692ad3ee
pm-pratyush <pm.pratyush.mohanty@gmail.com>
2024-01-17 18:21:29 +0530

    tested 2.cpp for small testcases from gfg

--- Assignment - 1/2/2.cpp
+++ Assignment - 1/2/2.cpp
@@ -5,12 +5,48 @@ using namespace std;
 
 #define MASTER 0
 #define WORKER 1
-
-#define ALIVE 1
-#define DEAD 0
-
-#define INV -1
 #define INF INT_MAX
+
+// Find the owner of the kth row
+int findOwner(vector<pair<int, int>> &row_segment, int k)
+{
+    for (int i = 0; i < row_segment.size(); ++i)
+    {
+        if (k >= row_segment[i].first && k <= row_segment[i].second)
+            return i;
+    }
+    return -1;
+}
+
+// Implement Floyd Warshall Algorithm
+void floydWarshall(vector<vector<int>> &graph, int N, int rank, int size, vector<pair<int, int>> &row_segment)
+{
+    for (int k = 0; k < N; ++k)
+    {
+        // Find the owner of the kth row
+        int owner = findOwner(row_segment, k);
+
+        // Broadcast the kth row to all processes
+        vector<int> kth_row(N, INF);
+        if (rank == owner)
+        {
+            kth_row = graph[k];
+        }
+        MPI_Bcast(&kth_row[0], N, MPI_INT, owner, MPI_COMM_WORLD);
+
+        // Update the graph
+        for (int i = row_segment[rank].first; i <= row_segment[rank].second; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+            {
+                if (graph[i][k] != INF && kth_row[j] != INF)
+                {
+                    graph[i][j] = min(graph[i][j], graph[i][k] + kth_row[j]);
+                }
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -47,100 +83,76 @@ int main(int argc, char *argv[])
         MPI_Bcast(&graph[i][0], N, MPI_INT, MASTER, MPI_COMM_WORLD);
     }
 
-    if (size == 1)
+    // Divide the rows among the workers
+    int workers = size;
+    int rows_per_worker = N / workers, remaining_rows = N % workers;
+
+    vector<pair<int, int>> row_segment(size);
+    row_segment[0] = {0, rows_per_worker - 1};
+    for (int i = 1; i < size; ++i)
     {
-        for (int k = 0; k < N; ++k)
+        int s_idx = row_segment[i - 1].second + 1;
+        int e_idx = s_idx + rows_per_worker - 1;
+        if (remaining_rows > 0)
         {
-            for (int i = 0; i < N; ++i)
+            ++e_idx;
+            --remaining_rows;
+        }
+        row_segment[i] = {s_idx, e_idx};
+    }
+
+    // Implement Floyd Warshall Algorithm
+    floydWarshall(graph, N, rank, size, row_segment);
+
+    // Exchange the updated rows
+    if (rank != MASTER)
+    {
+        // Send the updated rows to the master
+        vector<int> buffer;
+        for (int i = row_segment[rank].first; i <= row_segment[rank].second; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+                buffer.push_back(graph[i][j]);
+        }
+        MPI_Send(&buffer[0], buffer.size(), MPI_INT, MASTER, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        // Receive the updated rows from the workers
+        for (int i = 1; i < size; ++i)
+        {
+            int s_idx = row_segment[i].first, e_idx = row_segment[i].second;
+            vector<int> buffer((e_idx - s_idx + 1) * N);
+            MPI_Recv(&buffer[0], buffer.size(), MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            int index = 0;
+            for (int j = s_idx; j <= e_idx; ++j)
             {
-                for (int j = 0; j < N; ++j)
+                for (int k = 0; k < N; ++k)
                 {
-                    if (graph[i][k] != INF && graph[k][j] != INF && graph[i][k] + graph[k][j] < graph[i][j])
-                    {
-                        graph[i][j] = graph[i][k] + graph[k][j];
-                    }
+                    graph[j][k] = buffer[index++];
                 }
             }
         }
+    }
+
+    // Output the graph
+    if (rank == MASTER)
+    {
         for (int i = 0; i < N; ++i)
         {
-            cout << graph[i][0];
-            for (int j = 1; j < N; ++j)
+            for (int j = 0; j < N; j++)
             {
-                cout << " " << graph[i][j];
+                if (graph[i][j] == INF)
+                    cout << -1 << " ";
+                else
+                    cout << graph[i][j] << " ";
             }
             cout << endl;
         }
     }
-    else
-    {
-        if (rank == MASTER)
-        {
-            int deadProcesses = 0;
-            while (deadProcesses < size - 1)
-            {
-                int message[3];
-                MPI_Status status;
-                MPI_Recv(&message, 3, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-                int src = message[0], dest = message[1], weight = message[2];
-                if (status.MPI_TAG == DEAD)
-                {
-                    deadProcesses++;
-                }
-                else if (graph[src][dest] > weight)
-                {
-                    graph[src][dest] = weight;
-                }
-            }
-
-            for (int i = 0; i < N; ++i)
-            {
-                cout << graph[i][0];
-                for (int j = 1; j < N; ++j)
-                {
-                    cout << " " << graph[i][j];
-                }
-                cout << endl;
-            }
-        }
-        else
-        {
-            int rowsPerProcess = N / (size - 1);
-            int startRow = (rank - 1) * rowsPerProcess;
-            int endRow = (rank == size - 1) ? N : startRow + rowsPerProcess;
-
-            int message[3];
-            for (int k = 0; k < N; ++k)
-            {
-                for (int i = 0; i < N; ++i)
-                {
-                    for (int j = startRow; j < endRow; ++j)
-                    {
-                        if (graph[i][k] != INF && graph[k][j] != INF && graph[i][k] + graph[k][j] < graph[i][j])
-                        {
-                            graph[i][j] = graph[i][k] + graph[k][j];
-
-                            message[0] = i;
-                            message[1] = j;
-                            message[2] = graph[i][j];
-                            MPI_Send(&message, 3, MPI_INT, MASTER, ALIVE, MPI_COMM_WORLD);
-                        }
-                    }
-                }
-            }
-            MPI_Send(&message, 3, MPI_INT, MASTER, DEAD, MPI_COMM_WORLD);
-        }
-    }
-    
     // Finalize MPI
     MPI_Finalize();
     return 0;
 }
-
-// 5 
-// 0 4 -1 5 -1
-// -1 0 1 -1 6
-// 2 -1 0 3 -1
-// -1 -1 1 0 2
-// 1 -1 -1 4 0
