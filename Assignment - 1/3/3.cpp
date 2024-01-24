7a19dd07b9343202e1ab9823204f6c22b74ec904
pm-pratyush <pm.pratyush.mohanty@gmail.com>
2024-01-24 07:59:17 +0530

    mod3.cpp, works for given 3 TCs, not tested heavily

--- Assignment - 1/3/3.cpp
+++ Assignment - 1/3/3.cpp
@@ -7,12 +7,13 @@ using namespace std;
 #define WORKER 1
 
 // Function to go to the next generation
-void gotoNextGeneration(vector<vector<int>> &grid, int N, int M, int rs, int re)
+void gotoNextGeneration(vector<vector<int>> &grid, int NR, int NC, int rs, int re, int rank, int size)
 {
-    vector<vector<int>> nextGrid(N + 2, vector<int>(M + 2, 0));
-    for (int i = rs; i <= re; ++i)
+    // We only need to consider the rows from 1 to NR - 2 and columns from 1 to NC - 2
+    vector<vector<int>> nextGrid(NR, vector<int>(NC, 0));
+    for (int i = 1; i <= NR - 2; ++i)
     {
-        for (int j = 1; j <= M; j++)
+        for (int j = 1; j <= NC - 2; ++j)
         {
             int alive = 0;
             for (int di = -1; di <= 1; ++di)
@@ -24,6 +25,7 @@ void gotoNextGeneration(vector<vector<int>> &grid, int N, int M, int rs, int re)
                     alive += grid[i + di][j + dj];
                 }
             }
+
             if (grid[i][j] == 1)
             {
                 if (alive < 2 || alive > 3)
@@ -44,30 +46,30 @@ void gotoNextGeneration(vector<vector<int>> &grid, int N, int M, int rs, int re)
 }
 
 // Function to handle boundary exchange
-void handleBoundaryExchange(vector<vector<int>> &grid, int N, int M, int rs, int re, int rank, int size)
+void handleBoundaryExchange(vector<vector<int>> &grid, int NR, int NC, int rs, int re, int rank, int size)
 {
     int prev = rank - 1, next = rank + 1;
     if (rank % 2 == 0)
     {
-        if (rank > 1)
-            MPI_Recv(&grid[rs - 1][0], M + 2, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if (rank < size - 1)
-            MPI_Send(&grid[re][0], M + 2, MPI_INT, next, 0, MPI_COMM_WORLD);
-        if (rank < size - 1)
-            MPI_Recv(&grid[re + 1][0], M + 2, MPI_INT, next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if (rank > 1)
-            MPI_Send(&grid[rs][0], M + 2, MPI_INT, prev, 0, MPI_COMM_WORLD);
+        if (prev >= 0)
+            MPI_Send(&grid[1][0], NC, MPI_INT, prev, 0, MPI_COMM_WORLD);
+        if (next < size)
+            MPI_Send(&grid[NR - 2][0], NC, MPI_INT, next, 0, MPI_COMM_WORLD);
+        if (prev >= 0)
+            MPI_Recv(&grid[0][0], NC, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (next < size)
+            MPI_Recv(&grid[NR - 1][0], NC, MPI_INT, next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     else
     {
-        if (rank < size - 1)
-            MPI_Send(&grid[re][0], M + 2, MPI_INT, next, 0, MPI_COMM_WORLD);
-        if (rank > 1)
-            MPI_Recv(&grid[rs - 1][0], M + 2, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if (rank > 1)
-            MPI_Send(&grid[rs][0], M + 2, MPI_INT, prev, 0, MPI_COMM_WORLD);
-        if (rank < size - 1)
-            MPI_Recv(&grid[re + 1][0], M + 2, MPI_INT, next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (next < size)
+            MPI_Recv(&grid[NR - 1][0], NC, MPI_INT, next, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (prev >= 0)
+            MPI_Recv(&grid[0][0], NC, MPI_INT, prev, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (next < size)
+            MPI_Send(&grid[NR - 2][0], NC, MPI_INT, next, 0, MPI_COMM_WORLD);
+        if (prev >= 0)
+            MPI_Send(&grid[1][0], NC, MPI_INT, prev, 0, MPI_COMM_WORLD);
     }
 }
 
@@ -105,9 +107,30 @@ int main(int argc, char *argv[])
     MPI_Bcast(&M, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
     MPI_Bcast(&T, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
 
-    vector<vector<int>> grid(N + 2, vector<int>(M + 2, 0));
+    // Divide the rows among the workers
+    int workers = size;
+    int rows_per_worker = N / workers, extra_rows = N % workers;
+
+    vector<pair<int, int>> row_segment(size);
+    row_segment[0] = {1, rows_per_worker};
+    for (int i = 1; i < size; ++i)
+    {
+        int s_idx = row_segment[i - 1].second + 1;
+        int e_idx = s_idx + rows_per_worker - 1;
+        if (extra_rows > 0)
+        {
+            ++e_idx;
+            --extra_rows;
+        }
+        row_segment[i] = {s_idx, e_idx};
+    }
+
+    // Start the timer
+    start_time = MPI_Wtime();
+
     if (rank == MASTER)
     {
+        vector<vector<int>> grid(N + 2, vector<int>(M + 2, 0));
         for (int i = 1; i <= N; ++i)
         {
             for (int j = 1; j <= M; j++)
@@ -115,105 +138,120 @@ int main(int argc, char *argv[])
                 cin >> grid[i][j];
             }
         }
-    }
-    for (int i = 0; i < N + 2; ++i)
-    {
-        MPI_Bcast(&grid[i][0], M + 2, MPI_INT, MASTER, MPI_COMM_WORLD);
-    }
 
-    // Sync all the workers and start the timer
-    MPI_Barrier(MPI_COMM_WORLD);
-    start_time = MPI_Wtime();
-
-    if (size == 1)
-    {
-        for (int t = 0; t < T; ++t)
-        {
-            gotoNextGeneration(grid, N, M, 1, N);
-        }
-    }
-    else
-    {
-        // Divide the rows among the workers
-        int workers = size - 1;
-        int rows_per_worker = N / workers, extra_rows = N % workers;
-
-        vector<pair<int, int>> row_segment(size);
-        row_segment[0] = {0, 0};
+        // Send the rows to the workers
         for (int i = 1; i < size; ++i)
         {
-            int s_idx = row_segment[i - 1].second + 1;
-            int e_idx = s_idx + rows_per_worker - 1;
-            if (extra_rows > 0)
+            int s_idx = row_segment[i].first, e_idx = row_segment[i].second;
+
+            vector<int> sendBuffer;
+            for (int j = s_idx - 1; j <= e_idx + 1; ++j)
             {
-                ++e_idx;
-                --extra_rows;
+                for (int k = 0; k <= M + 1; ++k)
+                    sendBuffer.push_back(grid[j][k]);
             }
-            row_segment[i] = {s_idx, e_idx};
+            MPI_Send(&sendBuffer[0], sendBuffer.size(), MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+        int s_idx = row_segment[rank].first, e_idx = row_segment[rank].second;
+        vector<vector<int>> tempGrid(e_idx - s_idx + 3, vector<int>(M + 2, 0));
+        for (int i = s_idx - 1; i <= e_idx + 1; ++i)
+        {
+            for (int j = 0; j <= M + 1; ++j)
+            {
+                tempGrid[i - s_idx + 1][j] = grid[i][j];
+            }
         }
 
         // Run the simulation for T iterations
         for (int t = 0; t < T; ++t)
         {
-            if (rank == MASTER)
-            {
-                // Receive the result from the workers one by one
-                for (int i = 1; i < size; ++i)
-                {
-                    int s_idx = row_segment[i].first, e_idx = row_segment[i].second;
-
-                    // Receive the combined message from the ith worker
-                    vector<int> message((e_idx - s_idx + 1) * (M + 2));
-                    MPI_Recv(&message[0], message.size(), MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-                    // Unpack the message into the grid using the startRow and endRow
-                    int k = 0;
-                    for (int j = s_idx; j <= e_idx; ++j)
-                    {
-                        for (int l = 0; l < M + 2; ++l)
-                        {
-                            grid[j][l] = message[k++];
-                        }
-                    }
-                }
-            }
-            else
-            {
-                int s_idx = row_segment[rank].first, e_idx = row_segment[rank].second;
-
-                // Go to the next generation
-                gotoNextGeneration(grid, N, M, s_idx, e_idx);
-                // Handle boundary exchange
-                handleBoundaryExchange(grid, N, M, s_idx, e_idx, rank, size);
-
-                // Send the result to the master (pack the result into a single message)
-                vector<int> message;
-                for (int i = s_idx; i <= e_idx; ++i)
-                {
-                    for (int j = 0; j < M + 2; ++j)
-                    {
-                        message.push_back(grid[i][j]);
-                    }
-                }
-                // For each worker's iteration, send a single combined message to the master
-                MPI_Send(&message[0], message.size(), MPI_INT, MASTER, 0, MPI_COMM_WORLD);
-            }
-
-            // Synchronize all the workers
+            // Go to the next generation
+            gotoNextGeneration(tempGrid, e_idx - s_idx + 3, M + 2, s_idx, e_idx, rank, size);
+            // Handle boundary exchange
+            handleBoundaryExchange(tempGrid, e_idx - s_idx + 3, M + 2, s_idx, e_idx, rank, size);
+            // Sync all the processes
             MPI_Barrier(MPI_COMM_WORLD);
         }
-    }
 
-    // Sync all the workers and stop the timer
-    MPI_Barrier(MPI_COMM_WORLD);
-    end_time = MPI_Wtime();
+        // Copy the result back to the grid
+        for (int i = s_idx; i <= e_idx; ++i)
+        {
+            for (int j = 0; j <= M + 1; ++j)
+            {
+                grid[i][j] = tempGrid[i - s_idx + 1][j];
+            }
+        }
 
-    if (rank == MASTER)
-    {
+        // Receive the result from the workers one by one
+        for (int i = 1; i < size; ++i)
+        {
+            int s_idx = row_segment[i].first, e_idx = row_segment[i].second;
+
+            // Receive the combined message from the ith worker
+            vector<int> recvBuffer((e_idx - s_idx + 1) * (M + 2));
+            MPI_Recv(&recvBuffer[0], recvBuffer.size(), MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // Unpack the message into the grid using the startRow and endRow
+            int index = 0;
+            for (int j = s_idx; j <= e_idx; ++j)
+            {
+                for (int k = 0; k < M + 2; ++k)
+                {
+                    grid[j][k] = recvBuffer[index++];
+                }
+            }
+        }
+
+        // Stop the timer
+        end_time = MPI_Wtime();
+
         // Print the final grid
         printGrid(grid, N, M);
         // Print the time taken
         // cout << "Time taken: " << end_time - start_time << " seconds" << endl;
+    }
+    else
+    {
+
+        int s_idx = row_segment[rank].first, e_idx = row_segment[rank].second;
+        
+        // Receive the rows from the master
+        vector<int> recvBuffer((e_idx - s_idx + 3) * (M + 2));
+        MPI_Recv(&recvBuffer[0], recvBuffer.size(), MPI_INT, MASTER, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        // Unpack the message into the grid using the startRow and endRow
+        vector<vector<int>> tempGrid(e_idx - s_idx + 3, vector<int>(M + 2, 0));
+        int index = 0;
+        for (int i = 0; i <= e_idx - s_idx + 2; ++i)
+        {
+            for (int j = 0; j <= M + 1; ++j)
+            {
+                tempGrid[i][j] = recvBuffer[index++];
+            }
+        }
+
+        // Run the simulation for T iterations
+        for (int t = 0; t < T; ++t)
+        {
+            // Go to the next generation
+            gotoNextGeneration(tempGrid, e_idx - s_idx + 3, M + 2, s_idx, e_idx, rank, size);
+            // Handle boundary exchange
+            handleBoundaryExchange(tempGrid, e_idx - s_idx + 3, M + 2, s_idx, e_idx, rank, size);
+            // Sync all the processes
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+
+        // Send the result to the master
+        vector<int> sendBuffer;
+        for (int i = 1; i <= e_idx - s_idx + 1; ++i)
+        {
+            for (int j = 0; j <= M + 1; ++j)
+            {
+                sendBuffer.push_back(tempGrid[i][j]);
+            }
+        }
+        MPI_Send(&sendBuffer[0], sendBuffer.size(), MPI_INT, MASTER, 0, MPI_COMM_WORLD);
     }
 
     // Finalize MPI
